f8f540dcdc97574531d9f258dfce6e9fd04e0cf1
biglier <stas.leliuk@yandex.com>
2016-08-02 14:20:31 +0300

    Created Project

---
+++ SpreadSheet/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SpreadSheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTable
+            // 
+            this.dataGridViewTable.AllowUserToAddRows = false;
+            this.dataGridViewTable.AllowUserToOrderColumns = true;
+            this.dataGridViewTable.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridViewTable.Location = new System.Drawing.Point(1, 25);
+            this.dataGridViewTable.Name = "dataGridViewTable";
+            this.dataGridViewTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridViewTable.Size = new System.Drawing.Size(849, 674);
+            this.dataGridViewTable.TabIndex = 0;
+            this.dataGridViewTable.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTable_CellLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 711);
+            this.Controls.Add(this.dataGridViewTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTable;
+    }
+}
+
---
+++ SpreadSheet/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SpreadSheet
+{
+    public partial class Form1 : Form
+    {
+        List<string> Columns = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Thread ColumsThread = new Thread(CreateColums);
+            ColumsThread.Start();
+            dataGridViewTable.ColumnCount = 26;
+            AddingColumns(Columns);
+
+        }
+
+        private void dataGridViewTable_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            CheckCell();
+        }
+
+        private void CheckCell()
+        {
+
+        }
+
+        private void CreateColums()
+        {
+            if (Columns.Count < 1)
+            {
+                Columns.AddRange(GetAlphabeticalArray());
+            }
+            else
+            {
+                if (Columns.Count > 26)
+                {
+
+                }
+                else
+                {
+                    string last = Columns[Columns.Count-1];
+                    var array = GetAlphabeticalArray();
+                    for(int i=0 ; i < array.Count; i++)
+                    {
+                        array[i] = last + array[i];
+                    }
+                }
+            }
+        }
+
+        public List<string> GetAlphabeticalArray()
+        {
+            List<string> array = new List<string>();
+            for (char s = 'A'; s <= 'Z'; s++)
+            {
+                array.Add( s.ToString());
+            }
+            return array;
+        }
+
+        private void AddingColumns(List<string> columnsNames)
+        {
+            for(int i=0; i<columnsNames.Count; i++)
+            {
+                dataGridViewTable.Columns[i].Name = columnsNames[i];
+            }
+            
+        }
+    }
+}
